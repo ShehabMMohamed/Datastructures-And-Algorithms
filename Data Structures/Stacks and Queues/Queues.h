cbda10b5a8f796a5ce7055fd5fbbef389d56d678
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2019-01-06 17:08:13 +0200

    Almost done with Queues.

--- Data Structures/Stacks and Queues/Queues.h
+++ Data Structures/Stacks and Queues/Queues.h
@@ -1,7 +1,27 @@
 #ifndef QUEUES_H
 #define QUEUES_H
 
-class MyQueue {
+// Circular Array Implementation.
+class Queue {
+private:
+    int front, back, capacity;
+    int *A;
+public:
+    Queue();
+
+    int Front();
+
+    int Back();
+
+    void Enqueue(int);
+
+    int Dequeue();
+
+    bool isFull();
+
+    bool isEmpty();
+
+    ~Queue();
 
 };
 
--- Data Structures/Stacks and Queues/Stacks_Queues_demo.cpp
+++ Data Structures/Stacks and Queues/Stacks_Queues_demo.cpp
@@ -27,7 +27,7 @@ int main() {
         S.pop();
 
     // Using Queue.
-    //TODO.
+    Queue Q = Queue();
 
     return 0;
 }
