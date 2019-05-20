83b31d1fbf15b49b0d4c2ae378aaa1dbb670b7dc
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2019-05-20 21:57:19 +0200

    adding templates.

--- Data Structures/Stacks and Queues/Queues.h
+++ Data Structures/Stacks and Queues/Queues.h
@@ -1,30 +1,35 @@
 #ifndef QUEUES_H
 #define QUEUES_H
 
-// Circular Array Implementation.
+template <typename T>
 class Queue {
 private:
-    int front, back, capacity;
-    int *A;
+    int front_idx, back_idx, capacity;
+    T *A;
 public:
-    Queue();
-
-    int Front();
-
-    int Back();
-
-    void Enqueue(int);
-
-    int Dequeue();
-
-    bool isFull();
-
-    bool isEmpty();
-
-    void showQueue();
-
+    Queue() capacity(50), front_idx(-1), back_idx(-1) {
+        A = new T[capacity];
+    }
+    T Front() {
+        if(!isEmpty())
+            return A[front_idx];
+        std::cout<<"Error. Queue is empty.\n";
+    }
+    T Back() {
+        if(!isEmpty())
+            return A[back_idx];
+        std::cout<<"Error. Queue is empty.\n";
+    }
+    void Enqueue(const T& e);   //TODO.
+    T Dequeue();                //TODO;
+    bool isFull() {
+        return (front == end == (capacity - 1));
+    }
+    bool isEmpty() {
+        return (front == end == -1);
+    }
+    void showQueue();           //TODO;
     ~Queue();
 
 };
-
 #endif
