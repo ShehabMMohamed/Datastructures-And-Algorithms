e99d9d7a9dbeb198e1c9628ce5e3984d0dc6fada
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2019-06-09 00:41:51 +0200

    writing LinkedList Implementation

--- Data Structures/Linked Lists/LinkedLists.h
+++ Data Structures/Linked Lists/LinkedLists.h
@@ -1,25 +1,48 @@
 #ifndef LINKEDLISTS_H
 #define LINKEDLISTS_H
+#include "../Stacks and Queues/Stacks.h"
 
 template <typename T>
 class ListNode {
 public:
-    T data;
+    T val;
     ListNode* next;
-    ListNode(const T& item) : data (item), next (NULL) {};
+    ListNode() : next (NULL) {};
 };
 
 template <typename T>
 class LinkedList {
 private:
-    ListNode<T> *root;
-    ListNode *createNode(int data);
-    void deleteNode(const int pos);
+    ListNode* head;
+    ListNode* createNode(T data) {
+    	ListNode* node = new ListNode;
+    	node->val = data;
+    }
+    void deleteNode(int pos) {
+    	//TODO.
+    }
 
 public:
-    LinkedList() {};
-    ~LinkedList() {};
+    LinkedList() : root(NULL) {};
+    void InsertNode(const T& e) {
+    	//TODO.
+    };
+    void DeleteNode(const T& e) {
+    	//TODO.
+    };
+    void ReverseList() {
+    	Stack<T> s;
+    	ListNode* runner = head;
+    	while(runner) {
+    		s.push(runner->val);
+    		runner = runner->next;
+    	}
+    	runner = head;
+    	while(!s.isEmpty()) {
+    		//TODO.
+    	}
+    };
+    ~LinkedList() { delete head; };
 };
-
 
 #endif 
